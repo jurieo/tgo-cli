8451440f73a0fcc2e71f4a2977a048a2a6074702
jurieo <jurieo@jurieo.com>
2019-02-12 17:33:45 +0800

    新建项目
    新建项目

---
+++ service/webpack/webpack.dev.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const base = require('./webpack.base');
+const path = require('path');
+const config = require(path.resolve(`${process.cwd()}/config.json`));
+const loaders = require('./lib/loaders');
+
+// 开发模式
+base.mode = 'development';
+base.module = {rules: loaders('development')};
+base.devtool = 'cheap-module-eval-source-map';
+
+base.devServer = {
+  port: config.port,
+  overlay: {
+    errors: true
+  },
+  open: true,
+  hot: true,
+  stats:{
+    modules: false,
+    children: false,
+    chunks: false,
+    chunkModules: false
+  }
+};
+
+base.plugins.push(
+  new webpack.NamedModulesPlugin(),
+  new webpack.HotModuleReplacementPlugin()
+);
+
+module.exports = base;
