8c9b9e99808cbfa535ca40681d3af25b6b22d698
Jurieo <jurieo@jurieo.com>
2019-11-07 13:19:47 +0800

    font-size changed

--- template/pages/index.vue
+++ template/pages/index.vue
@@ -41,14 +41,14 @@ export default {
     "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
-  font-size: 2rem;
+  font-size: 350px;
   color: #35495e;
   letter-spacing: 1px;
 }
 
 .subtitle {
   font-weight: 300;
-  font-size: 1rem;
+  font-size: 25px;
   color: #526488;
   word-spacing: 5px;
   padding-bottom: 15px;
