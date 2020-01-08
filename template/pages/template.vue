2b73ca04bb559d702237179173868d27a678e76a
Jurieo <jurieo@jurieo.com>
2020-01-08 15:18:16 +0800

    加入template

---
+++ template/pages/template.vue
@@ -0,0 +1,17 @@
+<template></template>
+
+<script>
+let _this;
+export default {
+  head() {
+    return {
+      title: "模板页面"
+    };
+  },
+  mounted() {
+    _this = this;
+  }
+};
+</script>
+
+<style lang="less" scoped></style>
